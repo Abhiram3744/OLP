eea6a4078e536d85fc4c6db48473f103407cc60d
Abhiram3744 <abhiramaa0123@gmail.com>
2026-08-29 15:42:29 +0530

    nothing much imp

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173'
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173'
+  ]
 }));
 
 app.use(express.json());
